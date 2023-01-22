4c741a2d50c9bc34f4f89041dea9f4c6ad557550
TomascpMarques <tomascpmarques@gmail.com>
2023-01-22 15:54:44 +0000

    ai

--- src/Guesser.java
+++ src/Guesser.java
@@ -1,21 +1,9 @@
 public class Guesser {
     public BinaryTree treeA;
     public BinaryTree treeB;
-    public Double x;
-    public Double y;
 
     public void Starter() {
         this.treeA = new BinaryTree(Node.simpleConstructor(Math.random()));
         this.treeB = new BinaryTree(Node.simpleConstructor(Math.random()));
-    }
-
-    public Double Brain(BinaryTree a, BinaryTree b, Double x, Double y) {
-
-        return null;
-    }
-
-    public Boolean Hi_lo(Double target) {
-
-        return null;
     }
 }
